41b4c72315d6968fdc95f3e0f7df84c39ade912c
Wing Li <villagershrm@gmail.com>
2022-09-19 13:50:25 -0400

    start review7

---
+++ Review7/src/Q1StarTable.java
@@ -0,0 +1,17 @@
+/**
+ * Wing Li
+ * September 19, 2022
+ * Ask the user for the dimensions of a 2-d array, initialize all elements of said array to *
+ * Then, write it to a file named "star.txt"
+ */
+import java.util.Scanner;
+
+public class Q1StarTable {
+    public static void main(String[] args) throws Exception {
+        Scanner keyboard = new Scanner(System.in);
+        int len, width;
+        System.out.println("Input the dimensions of a table");
+        len = keyboard.nextInt();
+        width = keyboard.nextInt();
+    }
+}
