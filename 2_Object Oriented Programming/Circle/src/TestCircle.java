d16c8d4e6ec522a1f5cf4270203b3faae55c9a01
Wing Li <villagershrm@gmail.com>
2022-10-30 21:21:57 -0400

    comments

--- 2_Object Oriented Programming/Circle/src/TestCircle.java
+++ 2_Object Oriented Programming/Circle/src/TestCircle.java
@@ -2,15 +2,15 @@ public class TestCircle {
     public static void main(String[] args) {
         Circle c1 = new Circle(4, -1, 3);
         Circle c2 = new Circle(3, -2, 5);
-        Circle c3 = c1;
-        Circle c4 = new Circle(c1);
+        //Circle c3 = c1;
+        //Circle c4 = new Circle(c1);
 
         System.out.println(c1.area());
         System.out.println(c1.smaller(c2));
         if (c2.isInside(c1)) {
             System.out.println("C2 is entirely within c1");
         } else {
-            System.out.println("c2 is not entirely within c1");
+            System.out.println("c2 is not entirely within c1"); //Makes sense, seeing as c2 is bigger than c1
         }
 
         // c1 == c3 is true         -> Same memory address
